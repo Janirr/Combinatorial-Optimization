971aba06e6549942bcbb191b2538a7372dfdba6d
Janir <janir3g@gmail.com>
2022-12-10 20:22:26 +0100

    Added genetic algorithm

--- main.py
+++ main.py
@@ -20,86 +20,84 @@ def readFile(file_name):
         print("Could not open/read file:", file_name)
         sys.exit()
     n = int(f.readline())
-    tmp_tab = []
+    tmp = []
     for i in range(n):
         connection = list(map(int, f.readline().split(" ")))
-        tmp_tab.append(connection)
+        tmp.append(connection)
     f.close()
-    return tmp_tab
+    return tmp
 
 
-def DFSsolution(matrix):
+def DeepFirstSearch(tab):
     global minZ
-    if len(matrix) > 0:
+    if len(tab) > 0:
         z = 0
         path = [0]
-        visited = [False] * (len(matrix))
+        visited = [False] * (len(tab))
         minZ = 9999
         for i in range(len(visited)):
             visited[i] = False
         visited[0] = True
-        DFS(matrix, 1, path, visited, z)
+        DFS(tab, 1, path, visited, z)
 
 
-def DFS(matrix, current_position, path, visited, z):
+def DFS(tab, current_position, path, visited, z):
     global minZ
-    n = len(matrix)
-    if current_position == len(matrix):
-        if matrix[path[-1]][path[0]] != 0:
-            z += matrix[0][path[-1]]
+    if current_position == len(tab):
+        if tab[path[-1]][path[0]] != 0:
+            z += tab[0][path[-1]]
             path.append(0)
-            if z < minZ and len(path) > n:
+            if z < minZ and len(path) > len(tab):
                 minZ = z
                 print(path, z)
-            z -= matrix[0][path[-1]]
+            z -= tab[0][path[-1]]
             path.pop()
-    for v in range(len(matrix)):
-        if isSafe(matrix, v, path, current_position) and not visited[v]:
-            z += matrix[v][path[-1]]
+    for v in range(len(tab)):
+        if isSafe(tab, v, path, current_position) and not visited[v]:
+            z += tab[v][path[-1]]
             path.append(v)
             visited[v] = True
-            DFS(matrix, current_position + 1, path, visited, z)
+            DFS(tab, current_position + 1, path, visited, z)
             visited[v] = False
-            z -= matrix[v][path[-1]]
+            z -= tab[v][path[-1]]
             path.pop()
-            z -= matrix[v][path[-1]]
+            z -= tab[v][path[-1]]
 
 
 def randomGraph(n, max_value=1000):
-    edges = []
+    edgesList = []
     max_edges = n * (n - 1)
     for i in range(n):
         for j in range(n):
             if i != j:
                 edge = [i + 1, j + 1]
-                edges.append(edge)
+                edgesList.append(edge)
     # We are removing one by one values in reversed but sorted order.
     random_list = random.sample(range(1, max_edges), n)
-    sorted_tab = edges
     graph = [[0] * n for _ in range(n)]
-    for j in range(len(sorted_tab)):
-        x = sorted_tab[j][0] - 1
-        y = sorted_tab[j][1] - 1
+    for j in range(len(edgesList)):
+        x = edgesList[j][0] - 1
+        y = edgesList[j][1] - 1
         d = random.randint(1, max_value)
         graph[x][y] = d
         graph[y][x] = d
     return graph
 
 
-def nearestNeighbour(matrix):
+def nearestNeighbour(tab):
     path = [0]
     valuePaths = []
-    visited = [False] * (len(matrix))
+    visited = [False] * (len(tab))
     visited[0] = True
     minIndex = 0
     minValue = 9999
     z = 0
     i = 0
-    for _ in range(len(matrix) - 1):
-        for j in range(len(matrix)):
-            if minValue > matrix[i][j] > 0 and not visited[j]:
+    for _ in range(len(tab) - 1):
+        for j in range(len(tab)):
+            if minValue > tab[i][j] > 0 and not visited[j]:
                 minIndex = j
-                minValue = matrix[i][j]
+                minValue = tab[i][j]
         if minIndex != 0:
             path.append(minIndex)
             visited[minIndex] = True
@@ -108,25 +106,23 @@ def nearestNeighbour(matrix):
             i = minIndex
         minIndex = 0
         minValue = 9999
-    if matrix[path[-1]][0] > 0 \
-            and len(path) == len(visited) \
-            and len(valuePaths) + 1 == len(visited):
-        z += matrix[path[-1]][0]
-        valuePaths.append(matrix[path[-1]][0])
+    if tab[path[-1]][0] > 0 and len(path) == len(visited) and len(valuePaths) + 1 == len(visited):
+        z += tab[path[-1]][0]
+        valuePaths.append(tab[path[-1]][0])
         path.append(0)
         print(path, z)
 
 
-def lowestEdge(matrix):
-    n = len(matrix)
+def lowestEdge(tab):
+    n = len(tab)
     tmp = []
     path = []
     visited = [[False, False] for _ in range(n)]
     z = 0
     for i in range(n - 1):
         for j in range(i + 1, n):
-            if matrix[i][j] > 0:
-                block = [matrix[i][j], i, j]
+            if tab[i][j] > 0:
+                block = [tab[i][j], i, j]
                 tmp.append(block)
     tmp.sort()
     for i in range(len(tmp)):
@@ -156,30 +152,72 @@ def lowestEdge(matrix):
         print(path, z)
 
 
-def bruteForce(matrix):
-    n = len(matrix)
+def bruteForce(tab):
+    n = len(tab)
     pid = [l for l in range(n)]
     path = []
-    z = 99999999
+    z = 9999
     for i in itertools.permutations(pid):
         i = list(i)
         s = 0
         for j in range(1, len(i)):
-            s += matrix[i[j]][i[j - 1]]
-        s += matrix[i[j]][i[0]]
+            s += tab[i[j]][i[j - 1]]
+        s += tab[i[j]][i[0]]
         if s < z:
             z = s
             path = i
     return path, z
 
 
+def fitness(path, tab):
+    z = 0
+    for i in range(1, len(path)):
+        z += tab[path[i]][path[i - 1]]
+    return [z, path]
+
+
+def crossover(arr1, arr2, tab):
+    path1 = arr1[1]
+    path2 = arr2[1]
+    half = len(path1) // 2
+    new_path = []
+    for i in path1[:half]:
+        new_path.append(i)
+    for city in path2:
+        if city not in new_path:
+            new_path.append(city)
+    new_path.append(new_path[0])
+    calculated = fitness(new_path, tab)
+    return calculated
+
+
+def geneticSolution(tab, maxPopulation=10, mutations=5, iterations=5):
+    solutions = []
+    for i in range(iterations):
+        if i == 0:
+            for _ in range(maxPopulation):
+                path = random.sample(range(0, len(tab)), len(tab))
+                path.append(path[0])
+                solution = fitness(path, tab)
+                solutions.append(solution)
+        solutions.sort(reverse=True)
+        paths = solutions
+        for j in range(mutations):
+            solutions.append(crossover(paths[j], paths[j + 1], matrix))
+        solutions.sort()
+        solutions = solutions[:maxPopulation]
+    print(*solutions[0])
+
+
 if __name__ == "__main__":
-    for i in range(1, 2, 1):
+    for _ in range(1, 2, 1):
         matrix = readFile("test")
         print("Brute force:", bruteForce(matrix))
         print("--- DFS ---")
-        DFSsolution(matrix)
+        DeepFirstSearch(matrix)
         print("--- NNA ---")
         nearestNeighbour(matrix)
         print("--- LEA ---")
         lowestEdge(matrix)
+        print("--- GEN ---")
+        geneticSolution(matrix)
